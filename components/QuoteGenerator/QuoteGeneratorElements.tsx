8a301e67f25f59dbc79c5f551a0ec0dbd5f18557
Ruwani <ruwani054@gmail.com>
2023-11-04 12:10:48 +0530

    images added with styles

---
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import Link from "next/link";
+import Image from "next/image";
+import { exec } from "child_process";
+
+export const GradientBackgroundCon = styled.div`
+    background: linear-gradient(to right, #000046, #1cb5e0);
+    background-size: 400% 400%;
+    animation: gradient 6s ease infinite;
+    height: 100vh;
+    width: 100vw;
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+        background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+`;
+
+export const BackgroundImage1 = styled(Image)`
+  position: relative;
+  z-index: 1;
+  margin-left: -10px;
+  margin-top: -10px;
+`;
+
+export const BackgroundImage2 = styled(Image)`
+  position: fixed;
+  z-index: 1;
+  right: 0px;
+  bottom: -10px;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
+import { BackgroundImage1, BackgroundImage2, GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+import Clouds1 from '../assets/Clouds1.png'
+import Clouds2 from '../assets/thunderbolt-cloud.png'
 
 export default function Home() {
   return (
@@ -10,9 +13,21 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main}`}>
-        <div>hello</div>
-      </main>
+      <GradientBackgroundCon>
+
+        <BackgroundImage1
+        src={Clouds1} 
+        height="300"
+        alt="cloudbackground1"
+         />
+
+       <BackgroundImage2
+        src={Clouds2} 
+        height="300"
+        alt="cloudbackground2"
+         />
+
+      </GradientBackgroundCon>
     </>
   )
 }
